6c33d79ae99d2b2869c46398f4a6db215f9abfae
TalThulOrtAva <11299471+TalThulOrtAva@users.noreply.github.com>
2021-10-19 12:30:17 -0400

    well, I thought it would work.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  # take that, devise
-  def valid_password?(password)
-    if ::Rails.env == "development"
-      true
-    else
-      super
-    end
-  end
 end
